87de9f7cbe3f683d9a922adca12eab9f137ba733
Arthur De Jesus <philwebservices.pwsojt5@gmail.com>
2016-04-14 18:25:18 +0800

    Update

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,1 +1,13 @@
 
+(function(window, document, $) {
+    'use strict';
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1){
+            $('.navbar-menu').addClass('navbar-animated');
+        }
+        else{
+            $('.navbar-menu').removeClass('navbar-animated');
+        }
+    });
+
+})(window, document, jQuery);
