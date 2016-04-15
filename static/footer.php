341acbcb5e2393895c05bc5210548fa2342b03da
Philwebservices <Philwebservices@Philwebservicess-Mac-mini.local>
2016-04-15 14:26:18 +0800

    Update

---
+++ static/footer.php
@@ -0,0 +1,78 @@
+<footer>
+    <div class="section footer">
+        <div class="container">
+            <div class="grid-footer">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="grid-item">
+                            <h3 class="grid-title"><span class="red">GAS</span> TREATING</h3>
+
+                            <ul class="footer-list">
+                                <li><a href="#">Amine Plants</a></li>
+                                <li><a href="#">H2S Scavengers</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="grid-item">
+                            <h3 class="grid-title"><span class="red">GAS</span> Processing</h3>
+
+                            <ul class="footer-list">
+                                <li><a href="#"> Refrigeration Plants</a></li>
+                                <li><a href="#">JT Plants</a></li>
+                                <li><a href="#">NGL Storage Tanks</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="grid-item">
+                            <h3 class="grid-title"><span class="red">PRODUCTION</span> EQUIPMENT</h3>
+
+                            <ul class="footer-list">
+                                <li><a href="#"> Dehydration Units</a></li>
+                                <li><a href="#"> Aerial Coolers</a></li>
+                                <li><a href="#">Stabilizers</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="copyright-wrap">
+                <p>© 2016 Copyright - TransTex Treating</p>
+                <ul class="list-inline footer-menu">
+                    <li><a href="#">Privacy Policy </a></li>
+                    <li><a href="#">Terms </a></li>
+                    <li><a href="#"> Contact Us</a></li>
+                </ul>
+                <ul class="list-inline social-media">
+                    <li><a href="#"><span class="icon-facebook"></span></a></li>
+                    <li><a href="#"><span class="icon-twitter"></span></a></li>
+                    <li><a href="#"><span class="icon-youtube"></span></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="scripts/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+<script src="scripts/main.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='//www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+</script>
+</body>
+</html>
