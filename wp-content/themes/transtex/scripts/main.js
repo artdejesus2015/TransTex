a7af1d4483fe4bfac6af21a96302a5826d36368c
Arthur De Jesus <philwebservices.pwsojt5@gmail.com>
2016-04-25 18:38:39 +0800

    Update

--- wp-content/themes/transtex/scripts/main.js
+++ wp-content/themes/transtex/scripts/main.js
@@ -3,7 +3,6 @@
     'use strict';
 
 
-
     $(window).scroll(function() {
         if ($(this).scrollTop() > 1){
             $('.navbar-menu').addClass('navbar-animated');
@@ -50,6 +49,37 @@
 
 
 
-
-
 })(window, document, jQuery);
+
+/*
+ By Osvaldas Valutis, www.osvaldas.info
+ Available for use under the MIT License
+ */
+
+;( function ( document, window, index )
+{
+    var inputs = document.querySelectorAll( '.inputfile' );
+    Array.prototype.forEach.call( inputs, function( input )
+    {
+        var label	 = input.nextElementSibling,
+            labelVal = label.innerHTML;
+
+        input.addEventListener( 'change', function( e )
+        {
+            var fileName = '';
+            if( this.files && this.files.length > 1 )
+                fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+            else
+                fileName = e.target.value.split( '\\' ).pop();
+
+            if( fileName )
+                label.querySelector( 'span.label-upload' ).innerHTML = fileName;
+            else
+                label.innerHTML = labelVal;
+        });
+
+        // Firefox bug fix
+        input.addEventListener( 'focus', function(){ input.classList.add( 'has-focus' ); });
+        input.addEventListener( 'blur', function(){ input.classList.remove( 'has-focus' ); });
+    });
+}( document, window, 0 ));
